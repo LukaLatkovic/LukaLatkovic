f9cb080004f8ed51a61c41b1df4f71883359acf2
Luka Latkovic <latkovic.luka@gmail.com>
2026-02-15 22:02:16 +0100

    Changes in main page

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './core/theme.service';
+import { AboutSection } from './sections/about.component';
+import { ContactSection } from './sections/contact.component';
+import { ExperienceSection } from './sections/experience.component';
+import { HeroSection } from './sections/hero.component';
+import { SkillsSection } from './sections/skills.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroSection, ExperienceSection, SkillsSection, AboutSection, ContactSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,4 +17,6 @@ export class App implements OnInit {
   protected readonly title = signal('Luka Latković - Portfolio');
   constructor(public theme: ThemeService) {}
   ngOnInit() { this.theme.init(); }
+
+  date = new Date().getFullYear();
 }
